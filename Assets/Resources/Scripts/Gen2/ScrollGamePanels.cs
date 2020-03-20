d26537a67d99499726f2800e6b2b3d447d641ac2
AnW0lf <AngryWolfgang@gmail.com>
2020-03-20 16:59:45 +0300

    Scroll menu prototype

---
+++ Assets/Resources/Scripts/Gen2/ScrollGamePanels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollGamePanels : MonoBehaviour
+{
+    public RectTransform screens, bgImg;
+    public RectTransform[] btns;
+    public float swapDuration, bgImgXSizeOffset = 40f;
+    public LeanTweenType swapEase;
+
+    private RectTransform[] panels;
+
+    private void Awake()
+    {
+        panels = new RectTransform[screens.childCount];
+        for (int i = 0; i < panels.Length; i++)
+            panels[i] = screens.GetChild(i).GetComponent<RectTransform>();
+    }
+
+    public void Swap(int index)
+    {
+        if(index < panels.Length)
+        {
+            screens.LeanMoveLocalX(-panels[index].anchoredPosition.x, swapDuration).setEase(swapEase);
+            bgImg.LeanMoveLocalX(btns[index].anchoredPosition.x, swapDuration).setEase(LeanTweenType.easeInOutQuart);
+            bgImg.LeanSize(new Vector2(btns[index].sizeDelta.x + bgImgXSizeOffset, bgImg.sizeDelta.y), swapDuration);
+        }
+    }
+}
