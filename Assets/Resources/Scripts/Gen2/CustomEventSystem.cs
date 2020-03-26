2c0daba58c22fb0260e4a48e6138eb1d73450734
AnW0lf <AngryWolfgang@gmail.com>
2020-03-26 21:04:59 +0300

    Gen2 fix

---
+++ Assets/Resources/Scripts/Gen2/CustomEventSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomEventSystem : MonoBehaviour
+{
+    public static CustomEventSystem current { get; private set; } = null;
+
+    public Dictionary<string, bool> Local { get; private set; } = new Dictionary<string, bool>();
+    public Dictionary<string, bool> Global { get; private set; } = new Dictionary<string, bool>();
+
+    private void Awake()
+    {
+        if (current == null) current = this;
+        else if (current != this) Destroy(gameObject);
+    }
+
+
+}
